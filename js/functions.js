3ada32ff67b3dea3d5210922254c5b978293d954
JefersonEGomes <jefersonsportsempre@gmail.com>
2021-03-27 17:51:42 -0300

    adicionando funções e arquivos js

---
+++ js/functions.js
@@ -0,0 +1,98 @@
+
+// Funções para mundaça de informações na página
+function transicaoAvatar(){
+    document.getElementById("tituloId").innerHTML = "Avatar";
+    document.getElementById("descricaoId").innerHTML = "Apesar de não ter mais os movimentos da perna, o ex-fuzileiro naval Jake Sully ainda sente que pode ser um guerreiro. Sua intuição começa a se tornar realidade quando ele viaja anos-luz até a estação espacial montada no Planeta Pandora. Habitado por grandes seres azuis, os Na´vi, o local tem uma atmosfera fatal para qualquer terrestre. Por isso, oficiais criaram o programa Avatar, em que um corpo biológico, híbrido de humano e Na´vi, pode ser comandado a distância.";
+    document.getElementById("linkTrailer").href = "https://youtu.be/5PSNL1qE6VY";
+    document.getElementById("linkInfo").href = "https://www.themoviedb.org/movie/19995-avatar?language=pt-BR";
+    document.querySelector(".filme-principal").style.backgroundImage = "linear-gradient(rgba(0,0,0,.50),rgba(0,0,0,.50)100%), url('../img/Capa/avatarCapa.jpg')";
+    
+}
+
+
+function transicaoBlack(){
+    document.getElementById("tituloId").innerHTML = "Pantera Negra";
+    document.getElementById("descricaoId").innerHTML = "O príncipe T'Challa retorna a Wakanda para ser coroado rei. Assumindo o manto de Pantera Negra, ele vai à caça de um vilão que roubou um precioso metal de seu país.";
+    document.getElementById("linkTrailer").href = "https://youtu.be/wL4a4MafSjQ";
+    document.getElementById("linkInfo").href = "https://www.themoviedb.org/movie/284054-black-panther?language=pt-BR";
+    document.querySelector(".filme-principal").style.backgroundImage = "linear-gradient(rgba(0,0,0,.65),rgba(0,0,0,.65)100%), url('../img/Capa/blackPantherCapa.jpg')";
+    
+}
+
+function transicaoAvengersInfinity(){
+    document.getElementById("tituloId").innerHTML = "Vingadores: Guerra Infinita";
+    document.getElementById("descricaoId").innerHTML = "Homem de Ferro, Thor, Hulk e os Vingadores se unem para combater seu inimigo mais poderoso, o maligno Thanos. Em uma missão para coletar todas as seis pedras infinitas, Thanos planeja usá-las para infligir sua vontade maléfica sobre a realidade.";
+    document.getElementById("linkTrailer").href = "https://youtu.be/4jGRyEa2jhE";
+    document.getElementById("linkInfo").href = "https://www.themoviedb.org/movie/299536-avengers-infinity-war?language=pt-BR";
+    document.querySelector(".filme-principal").style.backgroundImage = "linear-gradient(rgba(0,0,0,.65),rgba(0,0,0,.65)100%), url('../img/Capa/avengersInfinityWarCapa.jpg')";
+    
+}
+
+function transicaoAvengersUltimato(){
+    document.getElementById("tituloId").innerHTML = "Vingadores: Ultimato";
+    document.getElementById("descricaoId").innerHTML = "Após os eventos devastadores de Vingadores: Guerra Infinita, o universo está em ruínas devido aos esforços do Titã Louco, Thanos. Com a ajuda de aliados remanescentes, os Vingadores devem se reunir mais uma vez a fim de desfazer as ações de Thanos e restaurar a ordem no universo de uma vez por todas, não importando as consequências.";
+    document.getElementById("linkTrailer").href = "https://youtu.be/PgrmbRID0eY";
+    document.getElementById("linkInfo").href = "https://www.themoviedb.org/movie/299534-avengers-endgame?language=pt-BR";
+    document.querySelector(".filme-principal").style.backgroundImage = "linear-gradient(rgba(0,0,0,.65),rgba(0,0,0,.65)100%), url('../img/Capa/avengersUltimatoCapa.jpg')";
+}
+
+function transicaoCivilWar(){
+    document.getElementById("tituloId").innerHTML = "Capitão América: Guerra Civil";
+    document.getElementById("descricaoId").innerHTML = "O ataque de Ultron fez com que os políticos buscassem algum meio de controlar os super-heróis, já que seus atos afetam toda a humanidade. Tal decisão coloca o Capitão América em rota de colisão com Tony Stark, o Homem de Ferro.";
+    document.getElementById("linkTrailer").href = "https://youtu.be/FRLYZn5PGuA";
+    document.getElementById("linkInfo").href = "https://www.themoviedb.org/movie/271110-captain-america-civil-war?language=pt-BR";
+    document.querySelector(".filme-principal").style.backgroundImage = "linear-gradient(rgba(0,0,0,.65),rgba(0,0,0,.65)100%), url('../img/Capa/civilWarCapa.jpg')";
+}
+
+function transicaoCroods2(){
+    document.getElementById("tituloId").innerHTML = "Os Croods 2: Uma Nova Era ";
+    document.getElementById("descricaoId").innerHTML= "Os Croods 2: Uma Nova Era é uma aventura sobre uma família pré-histórica que descobre as transformações no mundo enquanto tenta permanecer unida.";
+    document.getElementById("linkTrailer").href = "https://youtu.be/f4w4WKrtr00";
+    document.getElementById("linkInfo").href = "https://www.themoviedb.org/movie/529203-the-croods-a-new-age?language=pt-BR";
+    document.querySelector(".filme-principal").style.backgroundImage = "linear-gradient(rgba(0,0,0,.65),rgba(0,0,0,.65)100%), url('../img/Capa/croods2Capa.jpg')";
+}
+
+function transicaoGodzillaVsKong(){
+    document.getElementById("tituloId").innerHTML = "Godzilla vs. Kong";
+    document.getElementById("descricaoId").innerHTML = "Em uma época em que os monstros andam na Terra, a luta da humanidade por seu futuro coloca Godzilla e Kong em rota de colisão que verá as duas forças mais poderosas da natureza no planeta se confrontarem em uma batalha espetacular para as idades. Enquanto Monarch embarca em uma missão perigosa em terreno desconhecido e descobre pistas sobre as origens dos Titãs, uma conspiração humana ameaça tirar as criaturas, boas e más, da face da terra para sempre.";
+    document.getElementById("linkTrailer").href = "https://youtu.be/kIET9yMABD8";
+    document.getElementById("linkInfo").href = "https://www.themoviedb.org/movie/399566-godzilla-vs-kong?language=pt-BR";
+    document.querySelector(".filme-principal").style.backgroundImage = "linear-gradient(rgba(0,0,0,.65),rgba(0,0,0,.65)100%), url('../img/Capa/godzillaVsKongCapa.jpg')";
+}
+
+function transicaoRaya(){
+    document.getElementById("tituloId").innerHTML = "Raya e o Último Dragão";
+    document.getElementById("descricaoId").innerHTML = "O reino encantado Kumandra é dividido em cinco regiões e sua população venerava os dragões mágicos que eram presentes no reino, porém quando uma força maligna ameaçou a Terra, os dragões se sacrificaram para salvar a humanidade. Agora, 500 anos depois, o mesmo mal voltou e cabe a uma guerreira solitária, Raya, rastrear o lendário último dragão para restaurar a terra fraturada e seu povo dividido.";
+    document.getElementById("linkTrailer").href = "https://youtu.be/neJkyAziG1U";
+    document.getElementById("linkInfo").href = "https://www.themoviedb.org/movie/527774-raya-and-the-last-dragon?language=pt-BR";
+    document.querySelector(".filme-principal").style.backgroundImage = "linear-gradient(rgba(0,0,0,.65),rgba(0,0,0,.65)100%), url('../img/Capa/rayaCapa.jpg')";
+
+}
+
+function transicaoSmallville(){
+    document.getElementById("tituloId").innerHTML = "Smallville: As Aventuras do Superboy";
+    document.getElementById("descricaoId").innerHTML = "Antes da vida como Super-Homem, o jovem Clark Kent encara os problemas da adolescência enquanto aprende a controlar seus poderes e usá-los para proteger a cidade de Smallville.";
+    document.getElementById("linkTrailer").href = "https://www.imdb.com/video/vi1031380249?playlistId=tt0279600";
+    document.getElementById("linkInfo").href = "https://www.themoviedb.org/tv/4604-smallville?language=pt-BR";
+    document.querySelector(".filme-principal").style.backgroundImage = "linear-gradient(rgba(0,0,0,.65),rgba(0,0,0,.65)100%), url('../img/Capa/smallvilleCapa.jpg')";
+
+}
+
+function transicaoStarWars(){
+    document.getElementById("tituloId").innerHTML = "Star Wars: A Ascensão Skywalker";
+    document.getElementById("descricaoId").innerHTML = "Com o retorno do Imperador Palpatine, todos voltam a temer seu poder e, com isso, a Resistência toma a frente da batalha que ditará os rumos da galáxia. Treinando para ser uma completa Jedi, Rey ainda se encontra em conflito com seu passado e futuro, mas teme pelas respostas que pode conseguir a partir de sua complexa ligação com Kylo Ren , que também se encontra em conflito pela Força.";
+    document.getElementById("linkTrailer").href = "https://youtu.be/Cmgv3fwoxQw";
+    document.getElementById("linkInfo").href = "https://www.themoviedb.org/movie/181812-star-wars-the-rise-of-skywalker?language=pt-BR";
+    document.querySelector(".filme-principal").style.backgroundImage = "linear-gradient(rgba(0,0,0,.65),rgba(0,0,0,.65)100%), url('../img/Capa/starWarsCapa.jpg')";
+
+}
+
+function transicaoTomEJerry(){
+    document.getElementById("tituloId").innerHTML = "Tom & Jerry - O Filme";
+    document.getElementById("descricaoId").innerHTML = "Um gato de rua chamado Tom é contratado por uma garota chamada Kayla, uma jovem empregada que trabalha em um hotel glamouroso em Nova York, para se livrar de Jerry, um rato travesso que se mudou para o hotel, antes que ele seja a ruína de um importante casamento.";
+    document.getElementById("linkTrailer").href = "https://youtu.be/Jg-ls07nixc";
+    document.getElementById("linkInfo").href = "https://www.themoviedb.org/movie/587807-tom-jerry?language=pt-BR";
+    document.querySelector(".filme-principal").style.backgroundImage = "linear-gradient(rgba(0,0,0,.65),rgba(0,0,0,.65)100%), url('../img/Capa/tomEJerryCapa.jpg')";
+
+}
+
